8763837dd3608699eefc9d1bae2825dfb50af753
Hasan Bhuiyan <hasanbhuiyan503@gmail.com>
2026-07-13 23:47:32 +0600

    add catchAsync and sendResponse utils file

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,36 @@
+
 import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
-import config from "./config";
 import cors from "cors";
+import config from "./config";
 
-const app : Application = express();
 
-app.use(cors({
+
+
+
+
+const app: Application = express();
+
+app.use(
+  cors({
     origin: config.app_url,
     credentials: true,
-}))
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req:Request, res:Response) => {
-    res.send("Hello, Word")
-})
+
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World");
+});
+
+
+
+
+
 export default app;
